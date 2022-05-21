e6a3c4f55043ec7b3653c0b12fad5dc3a529a947
AmirsinaRashedi <amirsina.rashedi@gmail.com>
2022-05-22 02:58:56 +0430

    defined ApplicationContext and main

--- src/TicketAgencyApplication.java
+++ src/TicketAgencyApplication.java
@@ -1,5 +1,30 @@
+import util.ApplicationContext;
+
+import java.util.Scanner;
+
 public class TicketAgencyApplication {
     public static void main(String[] args) {
-
+        ApplicationContext applicationContext = new ApplicationContext();
+        while (true) {
+            System.out.println();
+            System.out.println("1- add passenger");
+            System.out.println("2- view seats");
+            System.out.println("3- exit");
+            System.out.print("choose: ");
+            Scanner intInput = new Scanner(System.in);
+            int choice = intInput.nextInt();
+            switch (choice) {
+                case 1:
+                    applicationContext.findSeat();
+                    break;
+                case 2:
+                    applicationContext.printSeats();
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.out.println("invalid input");
+            }
+        }
     }
 }
